8c8a60ac9720214c5959eeb60efb20fb852b90da
Bohdan Lysov <bohdan.lysov@dataart.com>
2024-01-22 20:46:36 +0200

    test

---
+++ scraper/index.js
@@ -0,0 +1,25 @@
+const prik = 1
+const kek2 = 5
+const mems = [].map((item) => item.lel)
+const obj = {
+    kek: () => {
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('sdasdd')
+        console.log('Smeth')
+    },
+    a: 'a',
+    b: {
+        kek: 'c',
+    },
+}
+
+console.log('prik')
